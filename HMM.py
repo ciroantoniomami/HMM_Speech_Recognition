9c3a9a567fbee43c54c5440e9de95975dacabb7f
ciroantoniomami <mamiciroantonio@gmail.com>
2021-06-06 16:57:53 +0200

    Create HMM file

---
+++ HMM.py
@@ -0,0 +1,35 @@
+import warnings
+import os
+import python_speech_features as mfcc
+from scipy.io import wavfile
+from hmmlearn import hmm
+import numpy as np
+
+warnings.filterwarnings('ignore')
+def extract_mfcc(full_audio_path):
+    sample_rate, wave =  wavfile.read(full_audio_path)
+    mfcc_feature = mfcc.mfcc(wave,sample_rate, 0.025, 0.01,20,nfft = 1200, appendEnergy = True)   
+    return mfcc_features
+
+def buildDataSet(dir):
+    # Filter out the wav audio files under the dir
+    fileList = [f for f in os.listdir(dir) if os.path.splitext(f)[1] == '.wav']
+    dataset = {}
+    for fileName in fileList:
+        label = fileName.split('.')[0]
+        #label = tmp.split('_')[1]
+        feature = extract_mfcc(dir+fileName)
+        if label not in dataset.keys():
+            dataset[label] = []
+            dataset[label].append(feature)
+        else:
+            exist_feature = dataset[label]
+            exist_feature.append(feature)
+            dataset[label] = exist_feature
+    return dataset
+
+
+
+if __name__ == "__main__":
+
+    buildDataSet("dataset")
